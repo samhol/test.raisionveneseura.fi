32844e4351700f03ee7adbf13b438736dd3c6efc
Sami Holck <sami.holck@gmail.com>
2017-03-01 16:59:56 +0200

    Updates

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ $menuBuilder = new MenuBuilder();
       src="https://code.jquery.com/jquery-2.2.4.min.js"
       integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
     crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.0/js/foundation.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.js"></script>
     <script>
       $(document).foundation();
     </script>
--- sivut/jasenlomake.php
+++ sivut/jasenlomake.php
@@ -5,20 +5,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo 'a'
+
 ?>
-<form data-abide>
-  <div class="name-field">
-    <label>Your name <small>required</small>
-      <input type="text" required pattern="[a-zA-Z]+">
-    </label>
-    <small class="error">Name is required and must be a string.</small>
+<h1>Jäsenhakemus lomake</h1>
+<form data-abide novalidate method="post">
+  <div data-abide-error class="alert callout" style="display: none;">
+    <p><i class="fi-alert"></i> Lomake sisältää virheitä</p>
   </div>
-  <div class="email-field">
-    <label>Email <small>required</small>
-      <input type="email" required>
-    </label>
-    <small class="error">An email address is required.</small>
+  <div class="row">
+    <div class="small-12 columns">
+      <label>Nimi <small class="alert">Vaadittu kenttä</small>
+        <input type="text" placeholder="Nimi" required pattern="number">
+        <span class="form-error">
+          Anna nimesi
+        </span>
+      </label>
+    </div>
+    <div class="small-12 columns">
+      <label>Sähköpostiosoite <small class="alert">Vaadittu kenttä</small>
+        <input type="email" placeholder="sähköpostiosoite" aria-describedby="exampleHelpTex" required pattern="email">
+      </label>
+      <span class="form-error">
+          Anna sähköpostiosoitteesi
+        </span>
+    </div>
   </div>
-  <button type="submit">Submit</button>
+  <div class="row">
+    <fieldset class="large-6 columns align-right">
+      <button class="button" type="submit" value="Submit">Lähetä</button>
+    </fieldset>
+    <fieldset class="large-6 columns">
+      <button class="button" type="reset" value="Reset">Tyhjennä</button>
+    </fieldset>
+  </div>
 </form>
