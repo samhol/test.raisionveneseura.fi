5720595908250781ecb9a21ca6442f09902c2a47
Sami Holck <sami.holck@gmail.com>
2017-03-08 07:48:31 +0200

    Updates

--- _srcs/templates/jasenlomake.php
+++ _srcs/templates/jasenlomake.php
@@ -1,11 +1,21 @@
 <?php
 
+use Zend\Mail;
+
 echo '<pre>';
 
 print_r($_POST);
-
-if ($_POST['email']) {
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+if ($email !== null) {
   echo 'erger';
+  var_dump($mail = new Mail\Message(),
+  $mail->setBody('This is the text of the email.'),
+  $mail->setFrom('jasenhakemus@raisionveneseura.fi'),
+  $mail->addTo('samiholck@gmail.com', 'Name of recipient'),
+  $mail->setSubject('TestSubject'),
+  $transport = new Mail\Transport\Sendmail(),
+  $transport->send($mail));
+  //  25rMxq~1VVtn
 }
 ?>
 </pre>
--- sivut/jasenlomake.php
+++ sivut/jasenlomake.php
@@ -35,12 +35,8 @@
 
 use Sphp\Stdlib\Path;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 require_once(Path::get()->local('_srcs/templates/jasenlomake.php'));
 
-print_r($_POST);
+//print_r($_POST);
 ?>
